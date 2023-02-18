95dba257f68a996876103f5716c911b022319435
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-02-19 01:37:06 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+let num3 = 20;
+let num4 = 8;
+let mod = num3 % num4;
+
+let set = [5, 12, 20, 3, 7];
+let max = Math.max(...set);
